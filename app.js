a0ac832ca3065d1da2a557db87bf8c629d62e912
soumya-901 <sparhi.bsp2018@gmail.com>
2022-07-06 09:59:49 -0400

    just change

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 
+app.get('/',(req,res)=>{
+    res.send("hello world");
+})
+
 const server= app.listen(PORT,()=>{
     console.log("server connected");
 })
